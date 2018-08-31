ee4f651545a0fd178db7b8c10a9b2057ea85c5cf
Diego Armando <Edu.Diegoarmando@hotmail.com>
2018-08-31 16:19:50 -0300

    port correction 2

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/Public'));
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('server running on port 3000');
 })
 
