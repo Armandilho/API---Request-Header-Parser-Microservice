0eb6f64e59f7ece48398ad8d568bb4130bdb4c5b
Diego Armando <Edu.Diegoarmando@hotmail.com>
2018-12-04 21:28:24 -0200

    update added

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
-// server.js
-const express = require('express');
+const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static(__dirname + '/Public'));
+app.use(express.static(__dirname + "/Public"));
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log('server running on port 3000');
-})
+app.listen(PORT, () => {});
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '../public', 'index.html'))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "../public", "index.html"))
 );
 
-app.get('/api/whoami', (req, res) => {
-  console.log('REQ_HEADERS:::: ', req.headers);
-  return res.json({ ipaddress: req.ip, language: req.headers['accept-language'],
-         software: req.headers['user-agent'] })
+app.get("/api/whoami", (req, res) => {
+  return res.json({
+    ipaddress: req.ip,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"]
+  });
 });
 
-app.use(function(req, res) {
-  res.send('404: Page not Found', 404);
+app.use((req, res) => {
+  res.send("404: Page not Found", 404);
 });
